1a22459ba968fb9e1b9ff4965b918c9693530c1b
Matthew Crnich <matthew.crnich@gmail.com>
2020-08-25 21:53:31 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,42 @@
+class Dog
+  def initialize(name, breed="Mutt")
+    @name = name
+    @breed = breed
+  end
+  
+  def name=(name)
+    @name = name
+  end
+  
+  def breed=(breed)
+    @breed = name 
+  end
+    
+  def name
+    @name
+  end
+  
+  def breed
+    @breed
+  end
+end
+    
+    
+# 1.	class Person
+# 2.	 
+# 3.	def initialize(first_name, last_name)
+# 4.	@first_name = first_name
+# 5.	@last_name = last_name
+# 6.	end
+# 7.	 
+# 8.	def name=(full_name)
+# 9.	first_name, last_name = full_name.split
+# 10.	@first_name = first_name
+# 11.	@last_name = last_name
+# 12.	end
+# 13.	 
+# 14.	def name
+# 15.	"#{@first_name} #{@last_name}".strip
+# 16.	end
+# 17.	 
+# 18.	end

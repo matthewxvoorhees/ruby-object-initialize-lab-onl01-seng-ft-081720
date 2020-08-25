8b3684a3711f634e480d3e242bbdb36b28f13943
Matthew Crnich <matthew.crnich@gmail.com>
2020-08-25 21:37:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -12,3 +12,22 @@ class Dog
   end
 end
     
+    
+#     1.	class Person
+# 2.	 
+# 3.	def initialize(first_name, last_name)
+# 4.	@first_name = first_name
+# 5.	@last_name = last_name
+# 6.	end
+# 7.	 
+# 8.	def name=(full_name)
+# 9.	first_name, last_name = full_name.split
+# 10.	@first_name = first_name
+# 11.	@last_name = last_name
+# 12.	end
+# 13.	 
+# 14.	def name
+# 15.	"#{@first_name} #{@last_name}".strip
+# 16.	end
+# 17.	 
+# 18.	end
